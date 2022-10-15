e0b26e17820e87f25562295aaf7deb95d1920507
kat <katarina@uab.edu>
2022-10-15 14:47:27 -0500

    Implemented SingletonStage class, App class, DashboardController class

---
+++ SingletonStage.java
@@ -0,0 +1,18 @@
+import javafx.stage.Stage;
+
+public class SingletonStage extends Stage {
+
+    private static SingletonStage stage = null;
+
+    private SingletonStage() {
+
+    }
+
+    public static SingletonStage getInstance() {
+        if (stage == null) {
+            stage = new SingletonStage();
+        }
+        return stage;
+    }
+
+}
